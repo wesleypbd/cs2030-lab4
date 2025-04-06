a9681816c089556642905910eedfdc3e64141251
Wesley Pribadi <E1356826@u.nus.edu>
2025-04-07 00:08:44 +0800

    Add files via upload

--- State.java
+++ State.java
@@ -4,17 +4,20 @@ public class State {
     private final PQ<Event> pq;
     private final Shop shop;
     private final Optional<Event> event;
+    private final String output;
 
     public State(PQ<Event> pq, Shop shop) {
         this.pq = pq;
         this.shop = shop;
         this.event = Optional.empty();
+        this.output = "";
     }
 
-    private State(PQ<Event> pq, Shop shop, Optional<Event> event) {
+    private State(PQ<Event> pq, Shop shop, Optional<Event> event, String output) {
         this.pq = pq;
         this.shop = shop;
         this.event = event;
+        this.output = output;
     }
 
     public Optional<State> next() {
@@ -29,17 +32,22 @@ public class State {
                     Shop newShop = nextPair.u();
 
                     return nextEvent
-                        .map(e -> new State(currPQ.add(e), newShop, Optional.of(event)))
-                        .or(() -> Optional.of(new State(currPQ, newShop, Optional.of(event))));
+                        .map(e -> new State(currPQ.add(e), newShop, Optional.of(event), event.toString()))
+                        .or(() -> Optional.of(new State(currPQ, newShop, Optional.of(event), event.toString())));
                 })
-        ).or(() -> Optional.of(new State(currPQ, shop, Optional.empty())));
+        ).or(() -> Optional.of(new State(currPQ, shop, Optional.empty(), output)));
     }
 
     public boolean isEmpty() {
-        return pq.isEmpty() && event.isEmpty();
+        return pq.isEmpty() && event.toString().isEmpty();
     }
 
     public Optional<Event> getEvent() {
         return event;
+    }
+
+    @Override
+    public String toString() {
+        return output;
     }
 }
